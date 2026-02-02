277679e09c22901a1c8c36baa240518e1389010a
Harsh-sd <harshsandilya05042002@gmail.com>
2026-02-02 17:24:49 +0530

    feat(auth): updated  middleware for JWT token validation

--- src/middleware.js
+++ src/middleware.js
@@ -1,25 +1,16 @@
+
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
 
+export function middleware(req) {
+  const token = req.cookies.get("access-token")?.value;
 
-export  async function middleware(req){
-    const token = req.cookies.get("access-token")?.value;
-    if(!token){
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
-    try{
-        const verifytoken=jwt.verify(token , process.env.SECRET_KEY);
-        if(!verifytoken){
-            return NextResponse.redirect(new URL("/login", req.url));
-        }
-        return NextResponse.next();
-       }
-    
-    catch(error){
-        return NextResponse.redirect(new URL("/login", req.url));
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
-    }
+  return NextResponse.next();
 }
+
 export const config = {
-  matcher: ["/dashboard/:path*"]
+  matcher: ["/dashboard"],
 };
